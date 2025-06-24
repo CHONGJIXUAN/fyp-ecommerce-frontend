6b77b3bb4c8582b3d5e5f91c51d64c26f1ba642f
LAPTOP-2M6TT3P2\yuz41 <intern3@ips.com.my>
2025-06-25 02:30:21 +0800

    Done the Cart Item and Checkout

--- src/customer/pages/Cart/PricingCard.tsx
+++ src/customer/pages/Cart/PricingCard.tsx
@@ -1,8 +1,37 @@
+import { Divider } from '@mui/material'
 import React from 'react'
 
 const PricingCard = () => {
+
+
   return (
-    <div>PricingCard</div>
+    <>
+      <div className='space-y-3 p-5'>
+          <div className='flex justify-between items-center'>
+            <span>Subtotal</span>
+            <span>100.00</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span>Discount</span>
+            <span>50</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span>Shipping</span>
+            <span>10.00</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span>Platform Fee</span>
+            <span>Free</span>
+          </div>
+      </div>
+
+      <Divider />
+
+      <div className='flex justify-between items-center p-5 text-primary'>
+          <span>Total</span>
+          <span>140.00</span>
+      </div>
+    </>
   )
 }
 
